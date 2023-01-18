4e09d6a17a04f6afa33150eabe6f27ad319c88d7
Azat Azmetov <azmetoff@gmail.com>
2023-01-18 17:54:29 +0300

    apiKey now gets from local.properties. created StockApi.kt

---
+++ app/src/main/java/com/azmetov/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,20 @@
+package com.azmetov.stockmarketapp.data.remote
+
+
+import com.azmetov.stockmarketapp.BuildConfig
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTINGS_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        private const val API_KEY = BuildConfig.API_KEY
+        private const val BASE_URL = "https://alphavantage.co"
+    }
+}
